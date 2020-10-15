380b3b8f9d73e5688996f57fa08659861b1bea80
Stanislav Lvovsky <stanislav.lvovsky@sap.com>
2020-10-15 14:50:53 +0300

    update logic

--- extension.js
+++ extension.js
@@ -3,7 +3,6 @@ const vscode = require("vscode");
 const _ = require("lodash");
 const Environment = require('yeoman-environment');
 
-
 function activate(context) {
 	context.subscriptions.push(vscode.commands.registerCommand("env.run.gen", function () {
 		testGen();
@@ -12,32 +11,27 @@ function activate(context) {
 	testGen();
 };
 
-
 async function testGen() {
-	const lookupEnv = Environment.createEnv();
-	const gensMeta = lookupEnv.lookup();
+	try {
+		const lookupEnv = Environment.createEnv();
+		const gensMeta = lookupEnv.lookup();
 
-	const name = await vscode.window.showQuickPick(lookupEnv.getGeneratorNames());
-	const namespace = `${name}:app`;
-
-	const genMeta = _.find(gensMeta, genMeta => {
-		return genMeta.namespace === namespace;
-	});
-
-	if (genMeta) {
-		const env = Environment.createEnv();
-		env.lookup({ packagePaths: genMeta.packagePath });
-
-		try {
+		const name = await vscode.window.showQuickPick(lookupEnv.getGeneratorNames());
+		if (name) {
+			const namespace = `${name}:app`;
+			const genMeta = _.filter(gensMeta, {namespace})[0];
+			
+			const env = Environment.createEnv();
+			env.lookup({packagePaths: genMeta.generatorPath});
+			
 			const gen = env.create(namespace);
 			env.runGenerator(gen);
 			vscode.window.showInformationMessage(_.get(gen, "_globalConfig.name", namespace) + " created and running ...");
-		} catch (error) {
-			vscode.window.showErrorMessage(error.stack);
 		}
+	} catch (error) {
+		vscode.window.showErrorMessage(error.stack);
 	}
 }
-
 
 module.exports = {
 	activate
