ee1b871acbfab8acb243016422f00d7c1f14bec3
Stanislav Lvovsky <stanislav.lvovsky@sap.com>
2020-10-15 10:31:44 +0300

    update logic

--- extension.js
+++ extension.js
@@ -1,44 +1,40 @@
 
-const vscode = require('vscode');
+const vscode = require("vscode");
+const _ = require("lodash");
 const Environment = require('yeoman-environment');
 
 
 function activate(context) {
-	context.subscriptions.push(vscode.commands.registerCommand("test-env-api.gen", function () {
-		testGenerator();
+	context.subscriptions.push(vscode.commands.registerCommand("env.run.gen", function () {
+		testGen();
 	}));
+
+	testGen();
 };
 
-async function testGenerator() {
-	try {
+
+async function testGen() {
+	const lookupEnv = Environment.createEnv();
+	const gensMeta = lookupEnv.lookup();
+
+	const name = await vscode.window.showQuickPick(lookupEnv.getGeneratorNames());
+	const namespace = `${name}:app`;
+
+	const genMeta = _.find(gensMeta, genMeta => {
+		return genMeta.namespace === namespace;
+	});
+
+	if (genMeta) {
 		const env = Environment.createEnv();
-		// lookup
-		await vscode.window.withProgress({
-			location: vscode.ProgressLocation.Notification,
-			title: "looking for generators..."
-		}, async () => {
-			return env.lookup();
-		});
-		// getGeneratorNames
-		const genName = await vscode.window.showQuickPick(env.getGeneratorNames());
-		if (genName) {
-			// create
-			const gen = await vscode.window.withProgress({
-				location: vscode.ProgressLocation.Notification,
-				title: `getting ${genName} generator...`
-			}, async () => {
-				return env.create(`${genName}:app`);
-			});
-			// runGenerator
-			const info = gen && gen._globalConfig && gen._globalConfig.name || genName;
-			vscode.window.showInformationMessage(`running '${info}' ...`);
-			env.runGenerator(gen);
-		}
-	} catch (error) {
-		console.error(error.stack);
-		vscode.window.showErrorMessage(error.stack);
+		env.lookup({ packagePaths: genMeta.packagePath });
+		const gen = env.create(namespace);
+		env.runGenerator(gen);
+
+
+		vscode.window.showInformationMessage(_.get(gen, "_globalConfig.name") + " created and running ...");
 	}
 }
+
 
 module.exports = {
 	activate
